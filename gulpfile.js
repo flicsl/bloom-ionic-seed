d4c61bd4e1a78505b839eb7bb13fd75360532cfb
Feliciano Santana <fsfelicianosantana@gmail.com>
2015-12-05 21:35:40 -0300

    Add index, style, script and templateCache functions

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,34 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var ngAnnotate = require('gulp-ng-annotate');
+var replace = require('gulp-replace');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var templateCache = require('gulp-angular-templatecache');
+var inject = require('gulp-inject');
+var htmlreplace = require('gulp-html-replace');
+var bowerFiles = require('main-bower-files');
+
+var outline = require('./outline.json');
+var endpoints = require('./endpoints.json');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
+var jsBundle = outline.dist + '/js/' + outline.name + '.min.js';
+var cssBundle = outline.dist + '/css/' + outline.name + '.min.css';
+
+var args = require('yargs')
+  .alias('p', 'prod')
+  .alias('i', 'int')
+  .default('prod', false)
+  .default('int', false)
+  .argv;
+
 gulp.task('default', ['sass']);
+gulp.task('build', ['templateCache', 'style', 'script', 'index']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -49,3 +71,70 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
+
+gulp.task('style', StyleTask);
+
+function StyleTask () {
+  return gulp.src(outline.src + '/**/*.css')
+              .pipe(concat(outline.name + '.min.css'))
+              .pipe(minifyCss())
+              .pipe(gulp.dest(outline.dist + '/css/'));
+}
+
+gulp.task('script', ScriptTask);
+
+function ScriptTask () {
+  return injectEndpoints(gulp.src(outline.src + '/**/*.js'))
+      .pipe(concat(outline.name + '.min.js'))
+      .pipe(ngAnnotate())
+      .pipe(replace('GR-APP-TITLE', outline.name))
+      .pipe(gulpif(args.prod, uglify()))
+      .pipe(gulp.dest(outline.dist + '/js/'));
+}
+
+function injectEndpoints (inputStream) {
+  var env;
+  if (args.prod || args.int)
+    env = 'prod';
+  else
+    env = 'dev';
+
+  var outputStream = inputStream;
+  for (var key in endpoints[env]) {
+    if (endpoints[env].hasOwnProperty(key)) {
+      outputStream = outputStream.pipe(replace(key, endpoints[env][key]));
+    }
+  }
+  return outputStream;
+}
+
+gulp.task('index', IndexTask);
+
+function IndexTask () {
+  var defaultInjectionOptions = {
+    addRootSlash: false,
+    ignorePath: '/' + outline.dist,
+    name: 'inject'
+  };
+
+  var bowerInjectionOptions = {
+    addRootSlash: false,
+    ignorePath: '/' + outline.dist,
+    name: 'bower'
+  };
+
+  return gulp.src(outline.src + '/index.html')
+        .pipe(htmlreplace({'appTitle': outline.name, 'templatingCache': 'js/templates.js'}))
+        .pipe(inject(gulp.src(bowerFiles(), {read: false}), bowerInjectionOptions))
+        .pipe(inject(gulp.src(jsBundle, {read: false}), defaultInjectionOptions))
+        .pipe(inject(gulp.src(cssBundle, {read: false}), defaultInjectionOptions))
+        .pipe(gulp.dest(outline.dist));
+}
+
+gulp.task('templateCache', TemplateCache);
+
+function TemplateCache () {
+  return gulp.src(outline.src + '/**/*.html')
+    .pipe(templateCache({standalone:true})).on('error', gutil.log)
+    .pipe(gulp.dest(outline.dist + '/js'));
+}
